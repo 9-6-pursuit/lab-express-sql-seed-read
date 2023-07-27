0770924a465d75b564b9e77b7b1b17e617220bfa
Lakisha J <lakishajohnson@pursuit.org>
2023-07-27 14:49:34 -0400

    Imported BrowserRouter, etc. Setting up initial route path

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,22 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import NavBar from "./Components/NavBar";
+
+import Index from "./Pages/Index";
+
 function App() {
   return (
-    <div className="">
-      <h1>Tuner App</h1>
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/songs" element={<Index />} />
+          </Routes>{" "}
+        </main>
+      </Router>
     </div>
   );
 }
