39f08ed4e1cf61ca2f62f0e6e68ad5d6c7559411
Lakisha J <lakishajohnson@pursuit.org>
2023-07-28 10:39:57 -0400

    Show Page

---
+++ front-end/src/Pages/Show.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ShowDetails from "../Components/ShowDetails";
+
+function Show() {
+  return (
+    <div className="Index">
+      <h2>Show</h2>
+      <ShowDetails />
+    </div>
+  );
+}
+
+export default Show;
