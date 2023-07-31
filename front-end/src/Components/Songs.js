dbb654e5832af59679272000820d94ed07d1d03b
Lakisha J <lakishajohnson@pursuit.org>
2023-07-31 15:12:55 -0400

    Added front-end logic for query search

--- front-end/src/Components/Songs.js
+++ front-end/src/Components/Songs.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const API = process.env.REACT_APP_API_URL;
 
 function Songs() {
   const [songs, setSongs] = useState([]);
+  const queryParams = useQuery();
+  const orderParam = queryParams.get("order");
+  const booleanParam = queryParams.get("is_favorite");
 
   useEffect(() => {
     axios
-      .get(`${API}/songs`)
+      .get(`${API}/songs`, {
+        params: {
+          order: orderParam,
+          is_favorite: booleanParam,
+        },
+      })
       .then((response) => setSongs(response.data))
       .catch((error) => console.warn("catch", error));
-  }, []);
+  }, [orderParam, booleanParam]);
 
   return (
     <div className="Songs">
@@ -29,8 +41,10 @@ function Songs() {
           </thead>
           <tbody>
             {songs.map((song, index) => (
-              <tr key={song.id}
-                className={index % 2 === 0 ? "row-even" : "row-odd"}>
+              <tr
+                key={song.id}
+                className={index % 2 === 0 ? "row-even" : "row-odd"}
+              >
                 <td>
                   {song.is_favorite ? (
                     <span>⭐️</span>
