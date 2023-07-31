13eb98a36a0593c45ac839a9b122a26029feb3e6
Lakisha J <lakishajohnson@pursuit.org>
2023-07-31 19:27:28 -0400

    Styled homepage

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 function Home() {
+  const backgroundUrl =
+    "https://www.nicepng.com/png/detail/1-16223_chromatic-musical-notes-typography-no-background-by-transparent.png";
+
   return (
-    <div className="Home">
-      <h1>Welcome to our Tuner App!</h1>
+    <div
+      className="Home"
+      style={{
+        backgroundImage: `url(${backgroundUrl})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        minHeight: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <h1 className="homepage">Welcome to our Tuner App!</h1> */}
     </div>
   );
 }
