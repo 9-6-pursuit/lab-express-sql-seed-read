c50b89d3d2e1fcf6d9258f7b9a020313bc68bab5
Lakisha J <lakishajohnson@pursuit.org>
2023-07-31 12:03:42 -0400

    Error page

---
+++ front-end/src/Pages/Error.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ErrorMsg from "../Components/ErrorMsg";
+
+function Error() {
+  return (
+    <div>
+        <ErrorMsg />
+    </div>
+  );
+}
+
+
+
+export default Error;
