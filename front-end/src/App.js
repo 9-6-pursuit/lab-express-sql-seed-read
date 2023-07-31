ec394009925183804d8d09c4c85307f0b73a2a4c
Lakisha J <lakishajohnson@pursuit.org>
2023-07-31 11:10:57 -0400

    Styling for entire app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,23 +2,43 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+// import NotFound from "./Components/NotFound";
 
 import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import New from "./Pages/New";
+import Edit from "./Pages/Edit";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={appStyle}>
       <Router>
         <NavBar />
-        <main>
+        <main style={mainStyle}>
           <Routes>
             {/* <Route path="/" element={<Home />} /> */}
             <Route path="/songs" element={<Index />} />
+            <Route path="/songs/new" element={<New />} />
+            <Route path="/songs/:id" element={<Show />} />
+            <Route path="/songs/:id/edit" element={<Edit />} />
           </Routes>{" "}
+          {/* <Route path="*" element={<NotFound />} /> */}
         </main>
       </Router>
+      <Footer />
     </div>
   );
 }
+
+const appStyle = {
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "100vh",
+};
+
+const mainStyle = {
+  flex: "1",
+};
 
 export default App;
